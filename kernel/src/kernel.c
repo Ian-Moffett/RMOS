851d8937d9d045776ea031cfd45ff4f62a9fe638
Ian-Moffett <hscorp99906@gmail.com>
2022-03-08 08:33:47 -0500

    msg

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -10,4 +10,13 @@ canvas_t canvas = {
 void _start(framebuffer_t* lfb, psf1_font_t* font) {
     canvas.lfb = lfb;
     canvas.font = font;
+
+    kwrite(&canvas, "Hello, World! This is the kernel speaking!\n", 0xFFFFFFFF);
+    kwrite(&canvas, "RED!\n", 0xFF0000);
+    kwrite(&canvas, "GREEN!\n", 0x00FF00);
+    kwrite(&canvas, "BLUE!\n", 0x0000FF);
+
+    while (1) {
+        __asm__ __volatile__("hlt");
+    }
 }
